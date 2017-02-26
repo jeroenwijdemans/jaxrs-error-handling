e9de5dd2193fc3f09472dab34bc9fd82c3fceeea
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2017-02-26 20:46:16 +0100

    move test to this package

---
+++ src/test/groovy/com/wijdemans/error/ErrorsTest.groovy
@@ -0,0 +1,19 @@
+package com.wijdemans.error
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import spock.lang.Specification
+
+class ErrorsTest extends Specification {
+
+    def "when is serialize Errors i expect intended json"() {
+        given:
+        Errors e = new Errors(new IllegalArgumentException("wrong input, bad people"))
+        ObjectMapper o = new ObjectMapper()
+
+        when:
+        def s = o.writeValueAsString(e)
+
+        then:
+        s == """{"errors":[{"code":100,"message":"wrong input, bad people","description":"wrong input, bad people"}]}"""
+    }
+}
