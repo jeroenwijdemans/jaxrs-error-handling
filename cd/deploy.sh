912ed00e2ec709bcf4b663aa1fe5fe72ed7dcd2e
jeroen wijdemans <jeroen.wijdemans@gmail.com>
2017-06-04 08:30:55 +0200

    fixed upload of jar files

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo 'deploy ... '
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-
+    openssl aes-256-cbc -K $encrypted_3fc0e7b86c25_key -iv $encrypted_3fc0e7b86c25_iv -in ./cd/codesigning.asc.enc -out ./cd/codesigning.asc -d
     gpg --fast-import ./cd/codesigning.asc
     ./gradlew uploadArchives -s
     echo "Finished uploading archives"
